00eab1314f505a8b262772458a086bef50ebd6a5
hanzheng98 <95270473+hanzheng98@users.noreply.github.com>
2024-07-09 15:26:41 -0500

    Update energy.py with cuda

--- pytorch_overhaul/energy.py
+++ pytorch_overhaul/energy.py
@@ -11,7 +11,6 @@ import datetime
 
 from torch.utils.tensorboard import SummaryWriter
 
-
 '''
 adding arguments here
 '''
@@ -19,20 +18,19 @@ adding arguments here
 parser = argparse.ArgumentParser()
 # default to be 12-spin lattice
 parser.add_argument('--device', type=str, default='cpu')
-parser.add_argument('--J', type=list[float, float], default=[1.0, 0.5])
+parser.add_argument('--J', type=list, default=[1.0, 0.5])
 parser.add_argument('--irrep', type=list, default=[6,6])
 parser.add_argument('--p', type=int, default=4)
 parser.add_argument('--num_sites', type=int, default=12)
-parser.add_argument('--numiter', type = int, default=500)
+parser.add_argument('--numiter', type=int, default=500)
 parser.add_argument('--lattice', type=str, default='rectangular')
 parser.add_argument('--sample_size', type=int, default=200)
 parser.add_argument('--lr', type=float, default=5e-3)
 parser.add_argument('--noise_scale', type=float, default=1e-5)
 parser.add_argument('--ham_scale', type=float, default=5e-3)
 args = parser.parse_args()
-args = parser.parse_args()
+
 snapshotdate = datetime.datetime.now().strftime('%m-%d_%H-%M')
-# os.makedirs('../data/'  + '/'+  snapshotdate + '/')
 comment = f'J2={args.J[1]} Type={args.lattice} LR={args.lr}'
 writer = SummaryWriter(log_dir= '../data/runs/' + snapshotdate + '/',comment=comment)
 
@@ -43,20 +41,17 @@ def expectation_energy(trial_wave, observable):
                                                  observable, trial_wave) 
     return torch.real(energy_expectation)
 
-def train(model, trial_state, optimizer, observable, multi_factor : int, device =None, debug=False):
+def train(model, trial_state, optimizer, observable, multi_factor: int, device=None, debug=False):
     model.train()                                                               
-    # total_loss = 0                                                               
     optimizer.zero_grad()
     x = model(trial_state)
-    observable = observable + (torch.ones(observable.shape[0])) * multi_factor
-    observable = torch.view_as_complex(torch.stack([observable, torch.zeros_like(observable)], dim=-1))
+    observable = observable + (torch.ones(observable.shape[0], device=device)) * multi_factor
+    observable = torch.view_as_complex(torch.stack([observable, torch.zeros_like(observable, device=device)], dim=-1))
     energy_expectation = torch.einsum('a, ab, b', torch.conj_physical(x),
                                                  observable, x)
-    # print(energy_expectation)
     if debug is False: 
         energy_expectation = torch.real(energy_expectation)
         energy_expectation.backward()
-        # optimizer.step()
         return energy_expectation
     elif debug:
         EDvalues, EDvectors = torch.linalg.eig(model.Heisenberg)
@@ -74,7 +69,7 @@ def main():
             [(2,5), (4,6), (6,9), (8,9), (11,12), (1,3),
             (2,4), (3,10), (2,6), (6,11), (6,12), (9,11),
             (9, 12), (5, 8), (1,7), (7, 10), (5,9)]]
-    elif args.lattice =='rectangular':
+    elif args.lattice == 'rectangular':
         lattice =[[(1,2), (1,8), (2,3), (2,7), (3,4), (3,6), (4,5), (5,6), (5, 12),
             (6,7), (6, 11), (7, 10), (7, 8), (8,9), (9, 10), (10, 11), (11, 12)],
 
@@ -83,47 +78,44 @@ def main():
             (9, 11), (10, 12)]]
     else: 
         raise NotImplementedError
+    
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
-    model = CQAFourier(args.J, args.num_sites, args.p, args.irrep, lattice, args.ham_scale ,debug=True).to(device)
-    # optimizer = SGD(model.parameters(), lr=args.lr)
+    
+    model = CQAFourier(args.J, args.num_sites, args.p, args.irrep, lattice, args.ham_scale, debug=True).to(device)
     optimizer = LBFGS(model.parameters(), lr=1)
-    scheduler = ReduceLROnPlateau(optimizer,mode='min', factor=0.5, patience=10, min_lr=0.000005)
-    observable = model.Heisenberg
-    EDvalues, EDvectors = torch.linalg.eig(model.Heisenberg)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, min_lr=0.000005)
+    observable = model.Heisenberg.to(device)
+    
+    EDvalues, EDvectors = torch.linalg.eig(model.Heisenberg.to(device))
     EDvector = EDvectors[torch.argmin(torch.real(EDvalues))]
     EDvalue = EDvalues[torch.argmin(torch.real(EDvalues))]
-    init_state = get_basis(model.dim, args.sample_size)
-    for i in range(1, args.numiter+1):
+    
+    init_state = get_basis(model.dim, args.sample_size).to(device)
+    
+    for i in range(1, args.numiter + 1):
         def closure():
             optimizer.zero_grad()
             x = model(init_state)
             loss = expectation_energy(x, observable)
             loss.backward()
-            # model.parameters() += torch.randn_like(model.layer.weight.grad)  
-            
             return loss 
+        
         lr = optimizer.param_groups[0]['lr']
-        # loss = train(model, init_state, optimizer, observable, args.device, debug=False)
-        # optimizer.step(closure(model, init_state, optimizer, observable, args.device, debug=False))
-        loss = train(model, init_state, optimizer, observable, len(lattice[0]) + len(lattice[1]) ,debug=False)
+        loss = train(model, init_state, optimizer, observable, len(lattice[0]) + len(lattice[1]), device=device, debug=False)
         optimizer.step(closure)
         loss2 = expectation_energy(model(init_state), observable)
-        # optimizer.step(closure)
+        
         with torch.no_grad():
-                for param in model.parameters():
-                    param.add_(torch.randn_like(param) * args.noise_scale)
+            for param in model.parameters():
+                param.add_(torch.randn_like(param) * args.noise_scale)
+        
         writer.add_scalar("Loss/train", loss.item(), i)
-        print(f'Iteration: {i:03d}, LR: {lr:.5f}, Loss: {loss2 : .8f}')
-        # writer.add_hparams({ "J2": args.J[1], 
-        # "Type": args.lattice, 
-        # "LR": args.lr}, {"loss": loss, "Iteration": args.numiter})
-    # EDvalues, EDvectors = torch.linalg.eig(model.Heisenberg)
-    # EDvector = EDvectors[torch.argmin(torch.real(EDvalues))]
-    # EDvalue = EDvalues[torch.argmin(torch.real(EDvalues))]
+        print(f'Iteration: {i:03d}, LR: {lr:.5f}, Loss: {loss2:.8f}')
+    
     print('ED energy: {}'.format(EDvalue))
     writer.flush()
     writer.close()
+
 if __name__ == "__main__":
     main()
-
